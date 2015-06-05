54454dd70994930311b86229000e53db04ba83e3
Mike Milford <mmilford85@gmail.com>
2015-06-04 22:02:20 -0500

    Add Parse Record functionality

--- GRCE.Domain/Models/Record.cs
+++ GRCE.Domain/Models/Record.cs
@@ -9,11 +9,6 @@
     [DataContract]
     public class Record
     {
-        public static ParseRecordResult CreateRecordFromString(string recordString, string[] delimiters)
-        {
-            return new ParseRecordResult(false, "Method Not Implemented", null);
-        }
-        
         public Record(string firstName, string lastName, Gender gender, string favoriteColor, DateTime dateOfBirth)
         {
             this.FirstName = firstName;
@@ -52,6 +47,85 @@
             {
                 return this.DateOfBirth.ToDobDisplayString();
             }
+        }
+
+        public static ParseRecordResult CreateRecordFromString(string recordString, string[] delimiters)
+        {
+            var recordVals = recordString.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            if (recordVals.Length != 5)
+            {
+                return ParseErrorResult(
+                    recordString,
+                    delimiters,
+                    string.Format("Record contains an invalid number ({0}) of fields", recordVals.Length));
+            }
+
+            var genderString = recordVals[2];
+            var gender = ParseGender(genderString);
+
+            if (gender == Enums.Gender.Unspecified)
+            {
+                return ParseErrorResult(
+                    recordString,
+                    delimiters,
+                    string.Format("Cannot parse gender from {0}", genderString));
+            }
+
+            var dobString = recordVals[4];
+            var dob = ParseDob(dobString);
+
+            if (dob == default(DateTime))
+            {
+                return ParseErrorResult(
+                    recordString,
+                    delimiters,
+                    string.Format("Cannot parse dob from {0}", dobString));
+            }
+
+            var firstName = recordVals[1];
+            var lastName = recordVals[0];
+            var favoriteColor = recordVals[3];
+
+            return new ParseRecordResult(
+                true,
+                string.Empty,
+                new Record(firstName, lastName, gender, favoriteColor, dob));
+        }
+
+        private static Gender ParseGender(string genderString)
+        {
+            var returnVal = Enums.Gender.Unspecified;
+
+            if (Enums.Gender.TryParse(genderString, true, out returnVal))
+            {
+                return returnVal;
+            }
+
+            return Enums.Gender.Unspecified;
+        }
+
+        private static DateTime ParseDob(string dobString)
+        {
+            var returnVal = default(DateTime);
+
+            if (DateTime.TryParse(dobString, out returnVal))
+            {
+                return returnVal;
+            }
+
+            return default(DateTime);
+        }
+
+        private static ParseRecordResult ParseErrorResult(string recordString, string[] delimterStrings, string errorMessage)
+        {
+            var fullErrorMessage = string.Format(
+                "Could not parse {0} with delimters {1}, Error Message: {2}",
+                recordString,
+                string.Join(", ", delimterStrings),
+                errorMessage);
+
+            return new ParseRecordResult(false, fullErrorMessage, null);
         }
     }
 }
